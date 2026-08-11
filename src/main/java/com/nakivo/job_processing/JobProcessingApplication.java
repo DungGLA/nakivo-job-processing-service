035c0cb3b64f1a91c15e3a8c33acf96ee0c0ec2b
dunggla <adung.hver23@gmail.com>
2026-08-11 16:07:17 +0700

    init job processing project

---
+++ src/main/java/com/nakivo/job_processing/JobProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.nakivo.job_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nakivo/job_processing/JobProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakivo.job_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
